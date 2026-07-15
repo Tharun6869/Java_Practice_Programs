d7751a87ec9e1ea6f8b74f0b07cfd7b0dcac269c
Tharun6869 <tharuntinku2005@gmail.com>
2026-07-15 16:58:33 +0530

    Changed class name and also modified the use input type

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class calculator {
+public class Calculator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the values");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
         System.out.println("Enter the operator(+,-,x,/,%):");
         char operator=sc.next().charAt(0);
         switch(operator){
@@ -19,17 +19,20 @@ public class calculator {
                 break;
             case '/':
                 if(b != 0)
-                    System.out.println("Result = " + (a / b));
+                    System.out.println("Division of numbers : " + (a / b));
                 else
                     System.out.println("Cannot divide by zero");
                 break;
             case '%':
-                System.out.println("Modulous of numbers:"+(a%b));
+                if(b != 0)
+                    System.out.println("Modulous of numbers:" + (a % b));
+                else
+                    System.out.println("Cannot modulous by zero");
                 break;
             default:
                 System.out.println("Enter correct operator");
 
         }
-
+        sc.close();
     }
 }
