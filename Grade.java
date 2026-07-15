ee11cf07c73798ebf858260a0c8f89da8ac51333
Tharun6869 <tharuntinku2005@gmail.com>
2026-07-15 16:24:42 +0530

    rechecked the marks from 1-100

--- Grade.java
+++ Grade.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
-public class Grade {
+
+public class Grade{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the marks");
-        int marks=sc.nextInt();
-        if(marks > 90)
-            System.out.println("Grade O");
-        else if(marks > 80)
-            System.out.println("Grade A+");
-        else if(marks > 70)
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        if(marks >= 90){
             System.out.println("Grade A");
-        else if(marks > 60)
-            System.out.println("Grade B+");
-        else if(marks > 50)
+        }
+        else if(marks >= 80){
             System.out.println("Grade B");
-        else if(marks == 50)
+        }
+        else if(marks >= 70){
             System.out.println("Grade C");
-        else
+        }
+        else if(marks >= 60){
+            System.out.println("Grade D");
+        }
+        else{
             System.out.println("Fail");
+        }
         sc.close();
     }
-    
 }
