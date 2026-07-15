4da2fa9a2997be46a1c918e33e72fc051dc5b665
Tharun6869 <tharuntinku2005@gmail.com>
2026-07-15 16:35:01 +0530

    removed ln inside loop also added scanner class to take user input

--- Multiplication.java
+++ Multiplication.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 class Multiplication{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number for multiplication table:");
+        int n=sc.nextInt();
         for(int i=1;i<=10;i++){
-            for(int j=1;j<=10;j++){
-                System.out.println(i+"x"+j+"="+(i*j)+" ");
-            }
-            System.out.println();
+            System.out.print(n+"x"+i+"="+(n*i)+"\n");
         }
+        sc.close();
     }
-
 }
 
