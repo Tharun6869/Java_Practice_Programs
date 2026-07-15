a8ce405cf7dc22721d531ab63c9165dbca54643f
Tharun6869 <tharuntinku2005@gmail.com>
2026-07-15 15:57:36 +0530

    updated scanner class and checked if the numbers are equal

--- CompareNum.java
+++ CompareNum.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
 public class CompareNum {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the two values:");
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
         if(a>b){
             System.out.println("A is largest");
         }
-        else{
+        else if(b>a){
             System.out.println("B is largest");
+        }
+        else{
+            System.out.println("Both are equal");
         }
     }
 }
